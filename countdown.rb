dacb7f833ccc200b989eb337e9a6d415dfd0203c
convivial-system-4430 <cdneal091@gmail.com>
2019-03-02 06:24:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,7 @@
 #write your code here
 
 def countdown(number)
-  while number >= 0 do
-    if number != 0
+  while number > 0 do
+    number == 1 ? puts "#{number} SECOND!" : puts "#{number} SECONDS!"
+    
 end
