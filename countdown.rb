b59624986552a1ef1c40ea99b9aa12887c7f00de
convivial-system-4430 <cdneal091@gmail.com>
2019-03-02 06:35:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,8 @@
 
 def countdown(number)
   while number > 0 do
-    number == 1 ? (puts "#{number} SECOND!" ): (puts "#{number} SECONDS!")
-    number -= 1
+    puts "#{number} SECOND(S)!"
+    number -=1
   end
-  puts "HAPPY NEW YEAR!"
   "HAPPY NEW YEAR!"
 end
